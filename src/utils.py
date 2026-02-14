bb2391aeba3bc9824d9e5afdb8e2f2b36876b64b
Yongbin Choi <whybe.choi@gmail.com>
2026-02-14 16:00:13 +0000

    feat: create utils

---
+++ src/utils.py
@@ -0,0 +1,43 @@
+import glob
+import json
+import os
+from typing import Tuple
+
+from safetensors import safe_open
+from transformers import AutoTokenizer
+
+from src.modeling_gemma import PaliGemmaForConditionalGeneration, PaliGemmaConfig
+
+
+def load_hf_model(
+    model_path: str, device: str
+) -> Tuple[PaliGemmaForConditionalGeneration, AutoTokenizer]:
+    # Load the tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_path, padding_side="right")
+    assert tokenizer.padding_side == "right"
+
+    # Fine all the *.safetensors files
+    safetensors_files = glob.glob(os.path.join(model_path, "*.safetensors"))
+
+    # ... and load them one by one in the tensors dictionary
+    tensors = {}
+    for safetensors_file in safetensors_files:
+        with safe_open(safetensors_file, framework="pt", device="cpu") as f:
+            for key in f.keys():
+                tensors[key] = f.get_tensor(key)
+
+    # Load the model's config
+    with open(os.path.join(model_path, "config.json"), "r") as f:
+        model_config_file = json.load(f)
+        config = PaliGemmaConfig(**model_config_file)
+
+    # Create the model using the configuration
+    model = PaliGemmaForConditionalGeneration(config).to(device)
+
+    # Load the state dict of the model
+    model.load_state_dict(tensors, strict=False)
+
+    # Tie wieghts
+    model.tie_weights()
+
+    return model, tokenizer
